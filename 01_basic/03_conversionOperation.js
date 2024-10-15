fff991597d990976d589f96d20556df4824c5b3f
lokendra888 <96539100+lokendra888@users.noreply.github.com>
2024-10-15 07:31:08 +0000

    operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,3 +1,4 @@
+// ********************Conversion*************
 let score = undefined
 
 // console.log(typeof score)
@@ -22,5 +23,38 @@ let booleanIsLoggin = Boolean(isLoggin)
 
 let someNumber = null
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// **************Operation ************
+
+let value =3
+let negValue = -value
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**3)
+// console.log(2-2)
+
+let str1 = "Hello lokendra"
+let str2 = " are you"
+let str3 = str1 + str2
+// console.log(str3)
+// console.log("2" + 5 );
+// console.log("5" + 5 +5);
+// console.log(5 +5 + "5");
+
+// console.log(+true); //1
+// console.log(true+); // error
+// console.log(+""); // 0
+
+let num , num1 , num2;
+num = num1 = num2 = 2+2
+// console.log(num1)
+
+let gameCounter = 100;
+// gameCounter++
+++gameCounter
+console.log(gameCounter);
+// link to study mdn prefix and postfix
