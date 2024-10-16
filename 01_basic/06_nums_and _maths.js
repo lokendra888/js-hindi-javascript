5fa3132cfdc712faeb89a1df527a321502c4bb4d
lokendra888 <96539100+lokendra888@users.noreply.github.com>
2024-10-16 05:45:26 +0000

    math and num

---
+++ 01_basic/06_nums_and _maths.js
@@ -0,0 +1,32 @@
+const score = 400
+// console.log(400)
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const otherNumber = 23.8655
+// console.log(otherNumber.toPrecision(4))
+
+const hundred = 100000
+// console.log(hundred.toLocaleString('en-IN'))
+
+
+// *****************Maths**************
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.2)) // round number 5
+// console.log(Math.ceil(4.2)) // uper 5
+// console.log(Math.floor(4.9)) // 4
+// console.log(Math.min(7,4,4,5,2,5,5))
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
+console.log((Math.random()*10) + 1)
+
+const min=10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min) + 1 )+ min)
