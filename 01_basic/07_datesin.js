7bd51700095011129966dea797f22b2f8422ae7b
lokendra888 <96539100+lokendra888@users.noreply.github.com>
2024-10-16 09:11:10 +0000

    date and day

---
+++ 01_basic/07_datesin.js
@@ -0,0 +1,24 @@
+let myDate = new Date();
+
+// console.log(myDate)
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString)
+// console.log(myDate.toString())
+// console.log(typeof myDate)
+
+// let myCreatedDate = new Date(2024,0,23,5,3);
+let myCreatedDate = new Date("2023-01-14");
+// console.log(myCreatedDate.toLocaleString())
+
+let myTime =Date.now();
+// console.log(myTime)
+// console.log(myCreatedDate.getTime())
+
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate =new Date()
+console.log(newDate)
+console.log(newDate.getMonth())
+console.log(newDate.toLocaleString('default',{
+    weekday:"long",
+}))
