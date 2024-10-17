b2c1263b9fc5630ad6a1d089fb97151c272b1f80
lokendra-birla <lokendra.birla@empower-solution.co.in>
2024-10-17 21:13:02 +0530

    scopes and function

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -5,13 +5,47 @@ if(true){
     let a =10
     const b = 20
     //  var c =30
-    console.log(a)
-    console.log(b)
+    // console.log(a)
+    // console.log(b)
 
 }
-
-
-
-console.log(a)
-console.log(b)
+// console.log(a)
+// console.log(b)
 // console.log(c)
+
+function one(){
+    const username= "Lokendra"
+    
+    function two(){
+        const website = "Youtube"
+        console.log(username)
+    }
+    // console.log(website)
+
+    two();
+}
+
+// one();
+
+if(true){
+    const username = "Lokendra"
+    if(username === "Lokendra"){
+        const website = "youtube"
+        // console.log(username + website)
+    }
+    // console.log(website)
+}
+
+// console.log(username)
+
+//++++++both are function intersting+++++++++++++++
+console.log(addOne(5)) // not errro
+function addOne(value){
+    return value + 1
+}
+
+console.log(addTwo(5)) //error
+const addTwo = function(num){
+    return num +2
+}
+
