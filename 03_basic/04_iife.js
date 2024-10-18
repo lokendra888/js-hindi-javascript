b1ad93ba202343fb449d51f8b32f68f31d2f9a52
lokendra-birla <lokendra.birla@empower-solution.co.in>
2024-10-18 17:17:50 +0530

    iife fixed

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,15 @@
+// Immediately Invoked Function Expressions IIFE 
+
+(function chai(){
+    // name iife
+    console.log(`DB connected`)
+})();
+
+(function chai(){
+    // name iife
+    console.log(`DB connected`)
+})();
+
+((user) =>{
+    console.log(`Db connected 44 ${user}`)
+})("lokendra")
