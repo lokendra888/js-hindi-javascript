b1ff88e0bac77b4934a8d4740040dbf6cc15e944
lokendra birla <lokendrab88@gmail.com>
2024-10-19 18:44:30 +0530

    iife

--- 03_basic/04_iife.js
+++ 03_basic/04_iife.js
@@ -1,15 +1,15 @@
 // Immediately Invoked Function Expressions IIFE 
 
-(function chai(){
+(function chai() {
     // name iife
     console.log(`DB connected`)
 })();
 
-(function chai(){
+(function chai() {
     // name iife
-    console.log(`DB connected`)
+    console.log(`DB connected two`)
 })();
 
-((user) =>{
+((user) => {
     console.log(`Db connected 44 ${user}`)
 })("lokendra")
